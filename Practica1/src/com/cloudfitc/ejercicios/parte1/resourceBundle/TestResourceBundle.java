7dcd331f471e17a212fcb1e3b88c7853291d2afc
devone_amedina <andres.medina@devone.es>
2022-10-05 20:23:56 +0200

    Test

--- Practica1/src/com/cloudfitc/ejercicios/parte1/resourceBundle/TestResourceBundle.java
+++ Practica1/src/com/cloudfitc/ejercicios/parte1/resourceBundle/TestResourceBundle.java
@@ -3,5 +3,6 @@ package com.cloudfitc.ejercicios.parte1.resourceBundle;
 public class TestResourceBundle {
     public static void main(String[] args){
         System.out.println("Hello Git World");
+        System.out.println("PracticasJava??");
     }
 }
