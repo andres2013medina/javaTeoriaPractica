c37c8145378431468423d8868cd5233f976cc8a9
devone_amedina <andres.medina@devone.es>
2022-10-05 20:22:28 +0200

    add ResourceBundle test

---
+++ Practica1/src/com/cloudfitc/ejercicios/parte1/resourceBundle/TestResourceBundle.java
@@ -0,0 +1,7 @@
+package com.cloudfitc.ejercicios.parte1.resourceBundle;
+
+public class TestResourceBundle {
+    public static void main(String[] args){
+        System.out.println("Hello Git World");
+    }
+}
